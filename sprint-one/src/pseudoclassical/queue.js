fe8e56209bd9039b0a4123fe46c9e2f4d6140563
jdbriner07 <jdbriner07@gmail.com>
2017-02-08 16:18:18 -0800

    Pseudoclassical patterns for stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.start = 0;
+  this.end = 0;
 };
 
-
+Queue.prototype.size = function() {
+  return this.length;
+};
+Queue.prototype.enqueue = function(value) {
+  this[this.end] = value;
+  this.length++;
+  this.end++;
+};
+Queue.prototype.dequeue = function() {
+  if (this.length > 0) {
+    this.length--;
+    var temp = this[this.start];
+    delete this[this.start];
+    this.start++;
+    return temp;
+  }
+};
